16d0e2eb362e480a152f35482324890f0675932f
amandinepo <98370509+amandinepo@users.noreply.github.com>
2023-03-28 11:46:30 +0200

    MAJ app.py

--- app.py
+++ app.py
@@ -3,32 +3,29 @@ from dash import Dash, dcc, html
 import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 values = pd.read_csv("values.csv")
 dates = pd.read_csv("dates.csv")
-d = {'values' : values, 'dates' : dates}
-df2 = pd.DataFrame(data=d)
-data = df2.query("type == 'conventional' and region == 'Albany'")
-data["dates"] = pd.to_datetime(data["dates"], format="%Y-%m-%d")
-data.sort_values("Date", inplace=True)
+d = {'values' : values.squeeze(), 'dates' : dates.squeeze()}
+df2 = pd.DataFrame(data = d)
 
 app = Dash(__name__)
 
 app.layout = html.Div(
      children=[
-        html.H1(children="Avocado Analytics"),
+        html.H1(children="Analyse des résultats"),
         html.P(
             children=(
-                "Analyze the behavior of avocado prices and the number"
-                " of avocados sold in the US between 2015 and 2018"
+                "Analyse du cours de l'indice du CAC 40"
             ),
         ),
         dcc.Graph(
             figure={
             "data": [
                     {
-                        "x": data['values'],
-                        "y": data['dates'],
+                        "x": df2['dates'],
+                        "y": df2['values'],
                         "type": "lines",
                     },
                 ],
